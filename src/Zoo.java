23afd333640e70a0131a0d5c0879ab4ece4db08f
malaktaboubi <taboubimalak@gmail.com>
2025-02-08 18:16:23 +0100

    adding prosit 3

--- src/Zoo.java
+++ src/Zoo.java
@@ -2,16 +2,61 @@ public class Zoo {
     private static final int MAX_ANIMALS = 25;
     private Animal[] animals;
     private String name;
+    public String getName() { return name; }
     private String city;
     private int nbrCages;
     private int animalCount;
 
-    public Zoo(String name, String city, int nbrCages) {
+    public Zoo(String name, String city) {
         this.name = name;
         this.city = city;
-        this.nbrCages = nbrCages;
+        this.nbrCages = MAX_ANIMALS;
         this.animals = new Animal[MAX_ANIMALS];
         this.animalCount = 0;
+    }
+
+    public boolean addAnimal(Animal animal) {
+        if (isZooFull() || searchAnimal(animal) != -1) {
+            return false;
+        }
+        animals[animalCount++] = animal;
+        return true;
+    }
+
+    public void displayAnimals() {
+        System.out.println("Animaux dans le zoo :");
+        for (int i = 0; i < animalCount; i++) {
+            System.out.println(animals[i]);
+        }
+    }
+
+    public int searchAnimal(Animal animal) {
+        for (int i = 0; i < animalCount; i++) {
+            if (animals[i].equals(animal)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public boolean removeAnimal(Animal animal) {
+        int index = searchAnimal(animal);
+        if (index == -1) {
+            return false;
+        }
+        for (int i = index; i < animalCount - 1; i++) {
+            animals[i] = animals[i + 1];
+        }
+        animals[--animalCount] = null;
+        return true;
+    }
+
+    public boolean isZooFull() {
+        return animalCount >= MAX_ANIMALS;
+    }
+
+    public static Zoo comparerZoo(Zoo z1, Zoo z2) {
+        return (z1.animalCount > z2.animalCount) ? z1 : z2;
     }
 
     public void displayZoo() {
--- src/ZooManagment.java
+++ src/ZooManagment.java
@@ -1,7 +1,6 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-
 public class ZooManagment {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -29,12 +28,33 @@ public class ZooManagment {
         System.out.print("Entrez la ville du zoo : ");
         String city = scanner.nextLine();
 
-        Zoo myZoo = new Zoo(zooName, city, nbrCages);
+        Zoo myZoo = new Zoo(zooName, city);
         Animal lion = new Animal("Felidae", "Lion", 5, true);
+        Animal tiger = new Animal("Felidae", "Tiger", 4, true);
+        Animal hedgehog = new Animal("Erinaceidae", "Hedgehog", 2, true);
+
+        System.out.println("Ajout Lion: " + myZoo.addAnimal(lion));
+        System.out.println("Ajout Tiger: " + myZoo.addAnimal(tiger));
+        System.out.println("Ajout Lion (test): " + myZoo.addAnimal(lion));
+
+        myZoo.displayAnimals();
+
+        System.out.println("Recherche Lion: " + myZoo.searchAnimal(lion));
+        System.out.println("Recherche Hedgehog: " + myZoo.searchAnimal(hedgehog));
+
+        System.out.println("Suppression Tiger: " + myZoo.removeAnimal(tiger));
+        Animal elephant = new Animal("Proboscidea", "Elephant", 15, true);
+        System.out.println("Ajout elephant: " + myZoo.addAnimal(elephant));
+        myZoo.displayAnimals();
 
         myZoo.displayZoo();
-        System.out.println(myZoo);
-        System.out.println(lion);
+
+
+        Zoo zoo2 = new Zoo("Wild Park", "Paris");
+        zoo2.addAnimal(new Animal("Giraffidae", "Giraffe", 7, true));
+        Zoo biggerZoo = Zoo.comparerZoo(myZoo, zoo2);
+        System.out.println("Le zoo avec le plus d'animaux est : " + biggerZoo.getName());
+
 
         scanner.close();
     }
